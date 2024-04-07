6246b33843227fe1fc708974f4820328363d63f4
GermanBertea <german@valarisolutions.com>
2024-04-07 09:04:58 +0200

    chap 3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    //Next.js can statically type links to prevent typos and other errors when using next/link, improving type safety when navigating between pages.
+    //https://nextjs.org/docs/app/building-your-application/configuring/typescript#statically-typed-links
+    experimental: {
+      typedRoutes: true,
+    },
+};
 
 module.exports = nextConfig;
